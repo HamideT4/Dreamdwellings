9af02e2d1b0f351046841312651017d77cbb8905
HamideT4 <hamidemahamat20@gmail.com>
2023-07-11 16:39:55 +0100

    Add controllers

---
+++ app/controllers/account_controller.rb
@@ -0,0 +1,10 @@
+class AccountController < ApplicationController
+  def index
+  end
+
+  def properties
+  end
+
+  def reports
+  end
+end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+    before_action :configure_devise_parameters, if: :devise_controller?
+
+    def configure_devise_parameters
+        devise_parameter_sanitizer.permit(:update, keys: [:name, :address, :phone, :description, :logo])
+        devise_parameter_sanitizer.permit(:sign_in, keys: [:name, :address, :phone, :description, :logo])
+    end
+
 end
